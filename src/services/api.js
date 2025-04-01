019e7866b21b1271f9bf5bdb81f45c56cece3061
vgayke <vgayke@binghamton.edu>
2025-04-01 18:15:31 -0400

    Add src folder with React components

---
+++ src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY = "77c956b9273ad006520365e2bdb80b9b";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
